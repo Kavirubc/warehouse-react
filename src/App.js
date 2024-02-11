37ddfd3cbe609511c26a588ee716f9cc5fe58f05
kaviru <hapuarachchikaviru@gmail.com>
2024-02-11 08:52:53 +0530

    Add login route to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/home';
 import Brands from './pages/brands';
 import './App.css';
@@ -14,7 +14,8 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/brands" element={<Brands />} />
         <Route path="/stocks" element={<Stocks />} />
         <Route path="/orders" element={<Orders />} />
